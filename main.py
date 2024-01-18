0f9c5a6b8fb2a0d730f23f6cd5448f3605ff03c1
noah-art3mis <costa.araujo.gustavo@gmail.com>
2024-01-17 23:57:47 -0300

    add dotenv and requirements

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-# https://github.com/simonw/llm
-# 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
+# https://llm.datasette.io/en/stable/index.html
 
 import llm
+import os
+import dotenv
+
+# MODEL = "3.5"
+MODEL = "mistral-7b-openorca"
+PROMPT = "does the api key go inside quotes insite the .env file?"
+
 
 def main():
-    pass
+    dotenv.load_dotenv()
+    model = llm.get_model(MODEL)
+    model.key = os.environ.get("LLM_OPENAI")
+    response = model.prompt(PROMPT)
+    print(response.text())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
